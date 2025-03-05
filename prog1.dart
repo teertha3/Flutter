f131a925635cd6d1f46bea8ecbbc32fb36c013d0
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-03-05 12:29:56 +0530

    Add files via upload

---
+++ prog1.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+void main(){
+stdout.write("Enter name:");
+String name=stdin.readLineSync()!;
+stdout.write("Enter age:");
+String age=stdin.readLineSync()!;
+print(name);
+print(age);
+}
+
